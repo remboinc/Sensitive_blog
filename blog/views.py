59fa838a5f1c2d2abf7939a109fa491a7db827ca
remboinc <remboinc@mail.ru>
2023-12-02 23:27:51 +0300

    replace all sorted calls with order_by on main page

--- blog/views.py
+++ blog/views.py
@@ -4,7 +4,7 @@ from blog.models import Comment, Post, Tag
 
 
 def get_related_posts_count(tag):
-    return tag.posts.count()
+    return tag.posts_count
 
 
 def get_likes_count(post):
@@ -39,9 +39,9 @@ def index(request):
     fresh_posts = Post.objects.order_by('published_at')
     most_fresh_posts = list(fresh_posts)[-5:]
 
-    tags = Tag.objects.all()
-    popular_tags = sorted(tags, key=get_related_posts_count)
-    most_popular_tags = popular_tags[-5:]
+    tags = Tag.objects.annotate(posts_count=Count('posts'))
+    popular_tags = tags.order_by('-posts_count')
+    most_popular_tags = popular_tags[:5]
 
     context = {
         'most_popular_posts': [
