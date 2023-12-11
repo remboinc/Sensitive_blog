82aa43b88c079ef12427cef09e4c7026a9b96d8e
Ekaterina Ryabova <58534082+remboinc@users.noreply.github.com>
2023-12-11 22:08:57 +0300

    Fix serialize_post

--- blog/views.py
+++ blog/views.py
@@ -16,13 +16,27 @@ def serialize_post(post):
         'title': post.title,
         'teaser_text': post.text[:200],
         'author': post.author.username,
+        'comments_amount': Post.objects.annotate(comments_count=Count('comments')),
+        'image_url': post.image.url if post.image else None,
+        'published_at': post.published_at,
+        'slug': post.slug,
+        'tags': [serialize_tag(tag) for tag in post.tags.all()],
+        'first_tag_title': post.tags.all()[0].title,
+    }
+
+
+def serialize_post_optimized(post):
+    return {
+        'title': post.title,
+        'teaser_text': post.text[:200],
+        'author': post.author.username,
         'comments_amount': len(Comment.objects.filter(post=post)),
         'image_url': post.image.url if post.image else None,
         'published_at': post.published_at,
         'slug': post.slug,
         'tags': [serialize_tag(tag) for tag in post.tags.all()],
         'first_tag_title': post.tags.all()[0].title,
-}
+    }
 
 
 def serialize_tag(tag):
