7d49e7ad8e52dce15bef66aa05d6f8f4b12773db
Ekaterina Ryabova <58534082+remboinc@users.noreply.github.com>
2023-12-26 11:40:15 +0300

    Delete 2 annotate

--- blog/views.py
+++ blog/views.py
@@ -47,17 +47,20 @@ def serialize_tag(tag):
 
 
 def index(request):
-    posts = Post.objects.annotate(
-        likes_count=Count('likes', distinct=True),
-        comments_count=Count('comments', distinct=True)
-    ).prefetch_related('author')
-    most_popular_posts = posts.order_by('-likes_count')[:5]
+    most_popular_posts = Post.objects.annotate(
+        likes_count=Count('likes', distinct=True)
+    ).order_by('-likes_count')[:5]
+    most_popular_posts_ids = [post.id for post in most_popular_posts]
+    posts_with_comments = Post.objects.filter(id__in=most_popular_posts_ids).annotate(comments_count=Count('comments'))
+    ids_and_comments = posts_with_comments.values_list('id', 'comments_count')
+    count_for_id = dict(ids_and_comments)
+    for post in most_popular_posts:
+        post.comments_count = count_for_id[post.id]
 
-    fresh_posts = Post.objects.order_by('published_at').annotate(
-        likes_count=Count('likes', distinct=True),
-        comments_count=Count('comments', distinct=True)
-    ).prefetch_related('author')
+    fresh_posts = Post.objects.order_by('published_at').prefetch_related('author')
     most_fresh_posts = list(fresh_posts)[-5:]
+
+
 
     tags = Tag.objects.annotate(
         posts_count=Count('posts', distinct=True),
